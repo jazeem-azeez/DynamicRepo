6730d65caffba96ca20f65dd90356d29c6b588dc
Jazeem Azeez <jazeem10@gmail.com>
2017-12-10 01:54:10 +0530

    controller check in

---
+++ DynamicRepo.Api/Controllers/DynamicRepoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DynamicRepo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class DynamicRepoController : Controller
+    {
+        private readonly IConfiguration config;
+
+        public DynamicRepoController(IConfiguration config)
+        {
+            this.config = config;
+        }
+        
+        [HttpGet("")]
+        public string Get()
+        {
+            var intro = config.GetValue<string>("DynamicRepoIntro");
+            return string.IsNullOrEmpty(intro)?"Dynamic Repo is a simple crud store wrapper":intro;
+        }
+    }
+
+}
